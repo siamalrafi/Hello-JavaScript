f7ed28a752198b3f4e8c69ffcdf0245269eb616a
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-13 19:21:51 +0600

    This is the python story

--- main.js
+++ main.js
@@ -25,4 +25,4 @@ var data = fs.readFile('input.txt',function(err, data){
 
 var fs = require("fs");
 var data = fs.readFile('input.txt',function(err, data){git add .
-    git commit -m"This is the python story"
+    git commit -m"This 
