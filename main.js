d2792d5a386f4c93b2d9fb9774c5df0bc25ec371
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-13 19:20:25 +0600

    This is the python story

--- main.js
+++ main.js
@@ -27,7 +27,6 @@ var data = fs.readFile('input.txt',function(err, data){
 console.log(data.toString());
 console.log("Program Ended");
 var fs = require("fs");
-var data 
 var fs = require("fs");
 var data = fs.readFile('input.txt',function(err, data){
     if(err) return 
