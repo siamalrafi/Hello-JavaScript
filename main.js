c235127210d67daa7e4337c650caebed502ca04e
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-13 19:19:07 +0600

    This is the python story

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ var fs = require("fs");
 
 fs.readFile('input.txt', function (err, data) {
     if (err) return console.error(err);
-    else { console.log(data.toString()); 
+    else { console.log(
