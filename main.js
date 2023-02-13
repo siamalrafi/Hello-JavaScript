76156bf0d90ccbbfc172a40244f07f0eb08e0100
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-13 19:20:54 +0600

    This is the python story

--- main.js
+++ main.js
@@ -22,15 +22,11 @@ console.log("Program Ended");
 var fs = require("fs");
 var data = fs.readFile('input.txt',function(err, data){
     if(err) return 
-});
 
-console.log(data.toString());
-console.log("Program Ended");
-var fs = 
 var fs = require("fs");
 var data = fs.readFile('input.txt',function(err, data){
     if(err) return 
 });
 
 console.log(data.toString());
-console.log("Program Ended");
+console.log("Program Ended"
