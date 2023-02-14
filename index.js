18b4c617727f7a035d1c8155304262b76dd7ab68
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-15 00:35:09 +0600

    This is the JavaScript Math Object

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const d = new Date("2021-03-25");
-let month = months[d.getMonth()];
-
-console.log(month);
+console.log('JavaScript Math Object');
