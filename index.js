5b933f000c2ff10f987754e1a860e27b44966aca
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-15 00:31:31 +0600

    This is the next

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const d = new Date("2021-03-25");
+let month = months[d.getMonth()];
 
-console.log('JavaScript Date Objects');
+console.log(month);
