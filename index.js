bfd7f4132a136b0673c64046a34a21de27e7f099
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-15 00:26:29 +0600

    This is hte JavaScript Date Objects

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-const numbers = [45, 4, 9, 16, 25];
 
-let x = 0;
-
-for (let i = 0; i < numbers.length; i++) {
-    x += numbers[i];
-
-};
-
-console.log(x);
+console.log('JavaScript Date Objects');
