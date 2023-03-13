417544aded71348737022501525928c9650d8ab9
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-03-14 00:19:34 +0600

    This is the Javascrpt

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("object");
+let text = '{ "employees" : [' +
+    '{ "firstName":"John" , "lastName":"Doe" },' +
+    '{ "firstName":"Anna" , "lastName":"Smith" },' +
+    '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+
+
+const obj = JSON.parse(text);
+
+
