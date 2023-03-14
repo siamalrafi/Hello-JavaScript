c588d27bdceeb3ff0ae243d69b01775c2f2592d6
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-03-14 19:58:43 +0600

    This is the next part

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-let text = '{ "employees" : [' +
-    '{ "firstName":"John" , "lastName":"Doe" },' +
-    '{ "firstName":"Anna" , "lastName":"Smith" },' +
-    '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
-
-
-
-const obj = JSON.parse(text);
-
-
+console.log("object");
